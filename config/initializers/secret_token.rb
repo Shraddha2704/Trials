3c6f26b2bd20ed177a63afdb51abd8bcecca5c50
Shraddha Chitalia <shraddhachitalia@Shraddhas-MacBook-Pro.local>
2015-06-17 17:15:02 -0700

    Empyty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trials::Application.config.secret_token = '7b2577bca0b7a153e9227200cf6c8143978feb22144c2f5ea6a7754db0ae9e97993e74d6ec1884869a5a7bd0fc855bfc40494557d7e5e0f532b9a7d234247674'
